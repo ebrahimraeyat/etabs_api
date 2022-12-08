ca94ecf9c6c62f074be4517ce770fcb41ab0d7b7
ebrahimraeyat <ebe79442114@yahoo.com>
2022-12-08 11:44:25 +0330

    minor change

--- test/test_load_combinations.py
+++ test/test_load_combinations.py
@@ -70,22 +70,22 @@ def test_get_load_combinations_of_type(shayesteh):
 def test_expand_load_combinations(two_earthquakes):
     ret = two_earthquakes.load_patterns.get_expanded_seismic_load_patterns()
     two_earthquakes.database.write_seismic_user_coefficient_df(ret[0], ret[2])
-    ex_loads = {
-        'EDRIFTX' : (('EXDRIFTX', 37)),
-        'EX1' : (('EX1', 5), ('EX1N', 5), ('EX1P', 5)),
-        'EX2' : (('EX2', 5), ('EX2N', 5), ('EX2P', 5)),
-        'EY1' : (('EY1', 5), ('EY1N', 5), ('EY1P', 5)),
-        'EY2' : (('EY2', 5), ('EY2N', 5), ('EY2P', 5)),
-        'EDRIFTY' : (('EDRIFTY', 37), ('EDRIFTYN', 37), ('EDRIFTYP', 37)),
-        }
+    # ex_loads = {
+    #     'EDRIFTX' : (('EXDRIFTX', 37)),
+    #     'EX1' : (('EX1', 5), ('EX1N', 5), ('EX1P', 5)),
+    #     'EX2' : (('EX2', 5), ('EX2N', 5), ('EX2P', 5)),
+    #     'EY1' : (('EY1', 5), ('EY1N', 5), ('EY1P', 5)),
+    #     'EY2' : (('EY2', 5), ('EY2N', 5), ('EY2P', 5)),
+    #     'EDRIFTY' : (('EDRIFTY', 37), ('EDRIFTYN', 37), ('EDRIFTYP', 37)),
+    #     }
     load_combos = two_earthquakes.load_combinations.expand_load_combinations(
-        expanded_loads = ex_loads,
+        expanded_loads = ret[1],
     )
     assert len(load_combos) == 32
-    assert load_combos[0][2][6] == 'EX1N'
-    assert load_combos[0][2][8] == 'EX2N'
-    assert load_combos[1][2][6] == 'EX1P'
-    assert load_combos[1][2][8] == 'EX2P'
+    assert load_combos[0][2][6] == 'EX1P'
+    assert load_combos[0][2][8] == 'EX2P'
+    assert load_combos[1][2][6] == 'EX1N'
+    assert load_combos[1][2][8] == 'EX2N'
     # assert load_combos == []
 
 if __name__ == '__main__':
