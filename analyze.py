81f57f9ab4021d6b5bc0862561517833996a12b8
ebrahimraeyat <ebe79442114@yahoo.com>
2023-03-14 18:20:53 +0330

    Set run flag to True for loadcases in automatic drift

--- analyze.py
+++ analyze.py
@@ -1,3 +1,6 @@
+from typing import Union
+
+
 __all__ = ['Analyze']
 
 
@@ -21,3 +24,14 @@ class Analyze:
                     self.SapModel.Analyze.SetRunCaseFlag(lc, False)
             else:
                 self.SapModel.Analyze.SetRunCaseFlag(lc, True) 
+    
+    def change_run_status_of_load_cases(self,
+        load_cases: Union[list, None]=None,
+        run_flag=True,
+        ):
+        all_load_cases = self.SapModel.Analyze.GetCaseStatus()[1]
+        if load_cases is None:
+            load_cases = all_load_cases
+        for lc in load_cases:
+            if lc in all_load_cases:
+                self.SapModel.Analyze.SetRunCaseFlag(lc, run_flag)
--- etabs_obj.py
+++ etabs_obj.py
@@ -392,12 +392,16 @@ class EtabsModel:
             x_loadcases=None,
             y_loadcases=None,
             ):
-        self.run_analysis()
         if loadcases is None:
             loadcases = self.etabs.load_cases.get_seismic_drift_load_cases()
         print(loadcases)
+        x_cases, y_cases = self.load_cases.get_xy_seismic_load_cases()
         if not x_loadcases:
-            x_loadcases, y_loadcases = self.load_cases.get_xy_seismic_load_cases()
+            x_loadcases = x_cases
+        if not y_loadcases:
+            y_loadcases = y_cases
+        self.analyze.change_run_status_of_load_cases(loadcases+x_loadcases+y_loadcases, True)
+        self.run_analysis()
         self.load_cases.select_load_cases(loadcases)
         TableKey = 'Diaphragm Max Over Avg Drifts'
         ret = self.database.read_table(TableKey)
