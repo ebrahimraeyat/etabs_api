fc0e5eb64df40620be8041a8e671bf0c79286598
ebrahimraeyat <ebe79442114@yahoo.com>
2023-12-31 20:51:16 +0330

    Check if diaphragm assigned to model

--- diaphragm.py
+++ diaphragm.py
@@ -11,3 +11,20 @@ class Diaphragm:
 
     def names(self):
         return self.SapModel.Diaphragm.GetNameList()[1]
+
+    def is_diaphragm_assigned(self):
+        table_key = 'Area Assignments - Diaphragms'
+        df = self.etabs.database.read(table_key, to_dataframe=True)
+        if df is not None:
+            filt = df['Diaphragm'] == 'None'
+            if len(df.loc[filt]) < len(df) / 2:
+                return True
+        table_key = 'Joint Assignments - Diaphragms'
+        df = self.etabs.database.read(table_key, to_dataframe=True)
+        if df is None:
+            return False
+        filt = df['Diaphragm'] == "From Shell Object"
+        if len(df.loc[filt]) < len(df) / 2:
+            return True
+        return False
+        
--- test/test_diaphragm.py
+++ test/test_diaphragm.py
@@ -13,3 +13,11 @@ def test_names():
     names = etabs.diaphragm.names()
     assert len(names) == 1
     assert set(names) == {'D1'}
+
+def test_is_diaphragm_assigned():
+    open_model(etabs=etabs, filename='shayesteh.EDB')
+    is_diaphragm_assigned = etabs.diaphragm.is_diaphragm_assigned()
+    assert is_diaphragm_assigned
+
+if __name__ == "__main__":
+    test_is_diaphragm_assigned()
