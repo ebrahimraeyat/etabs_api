cd797f3ef81fbb947c38c9840044b0c4532c8b77
ebrahimraeyat <ebe79442114@yahoo.com>
2024-02-05 16:24:22 +0330

    Flatten set function

--- python_functions.py
+++ python_functions.py
@@ -3,6 +3,9 @@
 def flatten_list(nested_list):
     return [item for sublist in nested_list for item in (flatten_list(sublist) if isinstance(sublist, list) else [sublist])]
 
+def flatten_set(nested_set):
+    return {element for subset in nested_set for element in (flatten_set(subset) if isinstance(subset, set) else {subset})}
+
 def is_text_in_list_elements(
         text_list: list,
         partial_text: str,
