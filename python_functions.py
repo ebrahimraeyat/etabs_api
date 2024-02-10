83613c4449523504269078114f891f3ababde036
ebrahimraeyat <ebe79442114@yahoo.com>
2024-02-10 15:33:13 +0330

    Add some python functions

--- python_functions.py
+++ python_functions.py
@@ -1,4 +1,4 @@
-
+from pathlib import Path
 
 def flatten_list(nested_list):
     return [item for sublist in nested_list for item in (flatten_list(sublist) if isinstance(sublist, list) else [sublist])]
@@ -12,3 +12,18 @@ def is_text_in_list_elements(
         ):
     matching_elements = [text for text in text_list if partial_text in text]
     return len(matching_elements) > 0
+
+
+def get_exe_path(program_name):
+    import subprocess
+    try:
+        result = subprocess.check_output(['where', program_name], shell=True, text=True)
+        return result.strip()
+    except subprocess.CalledProcessError:
+        return None
+    
+def get_temp_filepath(suffix='EDB', filename='test') -> Path:
+    import tempfile
+    temp_path = Path(tempfile.gettempdir())
+    temp_file_path = temp_path / f"{filename}.{suffix}"
+    return temp_file_path
